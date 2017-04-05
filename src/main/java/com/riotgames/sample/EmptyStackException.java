5723d66f2268b6e6868e621d52d9ac2707d5bafb
Jaehyeong <jo01jh@naver.com>
2017-04-06 03:15:34 +0900

    Delete EmptyStackException.java

--- src/main/java/com/riotgames/sample/EmptyStackException.java
+++
@@ -1,9 +0,0 @@
-public class EmptyStackException extends RuntimeException {
-  public EmptyStackException() {
-    super("The stack is empty.");
-  }
-  
-  public EmptyStackException(String message) {
-    super(message);
-  }
-}
